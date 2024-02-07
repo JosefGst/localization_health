49d0efbc8793194f43b736e9d8cd56320c14eebb
josefgst <josefgstoettner@live.at>
2024-02-07 15:22:44 +0800

    finish node

--- scripts/localization_health_node.py
+++ scripts/localization_health_node.py
@@ -2,20 +2,26 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
-cov_trhesh = rospy.get_param('cov_thresh', '0.2')
+cov_trhesh = rospy.get_param('cov_thresh', '0.4')
 
+health_pub = rospy.Publisher('localization_health_ok', Bool, queue_size=1)
 
 def callback(msg):
+    health_status = Bool()
+    health_status.data = True
     for covariance in msg.pose.covariance:
         # rospy.loginfo(rospy.get_caller_id() + "I heard %f", covariance)
         if abs(covariance) > float(cov_trhesh):
             rospy.loginfo("Covariance Threshold exceeded! STOP Robot.")
+            health_status.data = False
+            break
+
+    health_pub.publish(health_status.data)
     
 def localization_health():
     rospy.init_node('localization_health')
-    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
     rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, callback)
 
